4311924a2f4d2ff1655c88c912559bb46cb18357
Eray Diler <eray.diler@gmail.com>
2019-09-27 11:22:41 +0300

    Added a method to display requests sent by the slack bot

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.saveLinks = functions.https.onRequest((request, response) => {
+	if (request) {
+		console.log("Request body: " + JSON.stringify(request.body));		
+		console.log("Text: " + request.body.event.text);
+		response.status(200).send(request.body);
+	} else {
+		console.log("Request Error...");
+		throw response.status(500);
+	}
+});
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+	response.send("Hello from Firebase!");
 });
