bbdde5c71c679c95f3f6805a6321ce12c2a9cf50
Eray Diler <eray.diler@gmail.com>
2019-09-27 12:14:45 +0300

    Parsed links in slack message using node.js library `get-urls`

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,33 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
+// To parse links in texts
+const getUrls = require('get-urls');
+
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+// Gets messages in slack channel
+// Detects links in message
+// Saves links into db
 exports.saveLinks = functions.https.onRequest((request, response) => {
 	if (request) {
-		console.log("Request body: " + JSON.stringify(request.body));		
-		console.log("Text: " + request.body.event.text);
+		const body = request.body;
+		const text = request.body.event.text;
+		const bodyJSON = JSON.stringify(request.body);
+		const linksSet = getUrls(text);
+		const linksJSON = JSON.stringify(linksSet);
+
+		console.log("Request body: " + bodyJSON);		
+		console.log("Text: " + text);
+		// console.log("Links in text: " + linksJSON);
+
+		var iterator = linksSet.values();
+		for(let value of iterator) {
+		 	console.log("Link: " + value); 
+		}
+
 		response.status(200).send(request.body);
 	} else {
 		console.log("Request Error...");
